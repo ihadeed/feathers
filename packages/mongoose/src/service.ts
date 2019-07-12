a58331220ef09c1864119374c6b2117dce595bb0
Ibby Hadeed <ibby@zyra.ca>
2019-07-11 22:20:00 -0400

    Make base service options optional

--- packages/mongoose/src/service.ts
+++ packages/mongoose/src/service.ts
@@ -9,7 +9,7 @@ import { ERROR, errorHandler } from './error-handler';
 
 export { ERROR } from './error-handler';
 
-export interface MongooseServiceOptions<T extends mongoose.Document = any> extends ServiceOptions {
+export interface MongooseServiceOptions<T extends mongoose.Document = any> extends Partial<ServiceOptions> {
   Model: mongoose.Model<T>;
   lean?: boolean;
   overwrite?: boolean;
